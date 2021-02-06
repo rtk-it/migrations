658de92df1e369f2dd5b17946ac925ffab76af5b
Roman Gordeev <roma.gordeev@gmail.com>
2021-02-06 14:04:21 +0300

    initial commit

---
+++ src/test/java/ru/rgordeev/migrations/MigrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rgordeev.migrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
